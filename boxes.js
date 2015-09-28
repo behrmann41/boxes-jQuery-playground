c663bcd67779fbaf08676df9872a1c0fd4b904ef
Ian Behrmann <ianjbehrmann@gmail.com>
2015-09-28 11:11:15 -0600

    exercises 1 & 2 complete

--- boxes.js
+++ boxes.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   alert('Document Ready!')
+  //$('#secretBox').css('background-color', 'white').html("secret box!")
+  //$('#row1 > div').removeClass().addClass('box boxType3')
+  //$('#row4 > div:last').hide()
+  //$('.boxType1').css('background-color', 'white')
+  //$('#row2 :first, #row2 :eq(1)').removeClass()
+  $('div').not('#container').not('.row').not('#secretBox').css('width', '2px');
 })
